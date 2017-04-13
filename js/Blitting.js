d0fbdcfb468b98946dcb3c282545573109906c94
Ashleigh Morris <dylemacgregor@gmailcom>
2017-04-12 21:39:11 -0400

    A better Blit Demostration

--- js/Blitting.js
+++ js/Blitting.js
@@ -1,19 +1,15 @@
-var srcX, srcY, copyW, copyH, destX, destY;
-srcX = 200;
-srcY = 50;
-copyW = 150; //canvas.width/2;
-copyH = 150; //canvas.height/2;
-destX = 300; //canvas.width/2;
-destY = 250; //canvas.height/2;
-
 function basicBlit(){
-  hiddenCanvasContext.drawImage(canvas, destX, destY, copyW, copyH,
-    srcX, srcY, copyW, copyH);
+  var srcX, srcY, copyW, copyH, destX, destY;
+  srcX = 200;
+  srcY = 50;
+  copyW = 150; //canvas.width/2;
+  copyH = canvas.height; //canvas.height/2;
+  destX = 300; //canvas.width/2;
+  destY = 250; //canvas.height/2;
+
+  hiddenCanvasContext.drawImage(canvas, destX, destY, copyW, copyH, 0, 0, copyW, copyH );
   canvasContext.drawImage(canvas, srcX, srcY, copyW, copyH,
     destX, destY, copyW, copyH);
-
-  /*
   canvasContext.drawImage(hiddenCanvas, 0, 0, copyW, copyH,
     srcX, srcY, copyW, copyH);
-  */
 }
