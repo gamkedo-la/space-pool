4cda4173cd1098d9d4cae646094d14378d7d58f2
Sdonai <silentstrum@gmail.com>
2017-04-13 21:23:37 -0700

    Experiment with blitting performance

--- js/Asteroid.js
+++ js/Asteroid.js
@@ -136,7 +136,7 @@ function Asteroid(max_radius) {
     }
     this.isReadyToRemove = true;
     asteroidsHit++;
-    var tempBlit = new Blit(randomInteger(1, 600), 0, randomInteger(1, 150), canvas.height, 0, 0);
+    var tempBlit = new Blit(randomInteger(1, 600), 0, randomInteger(1, 150), canvas.height, randomInteger(1, 600), 0);
     blits.push(tempBlit);
   };
 
--- js/Blitting.js
+++ js/Blitting.js
@@ -16,30 +16,33 @@ function clearAllBlits(){
 
 function Blit(srcX, srcY, copyW, copyH, destX, destY){
   this.blitTimer = 0;
-
+  this.lifespan = 60
   this.srcX = srcX;
   this.srcY = srcY;
   this.copyW = copyW;
   this.copyH = copyH;
   this.destX = destX;
   this.destY = destY;
-
   this.reset = function(){};
 
   this.move = function(){
   }
 
   this.draw = function(){
-    hiddenCanvasContext.drawImage(canvas, this.destX, this.destY, this.copyW, this.copyH, 0, 0, this.copyW, this.copyH );
+    //hiddenCanvasContext.drawImage(canvas, this.destX, this.destY, this.copyW, this.copyH, 0, 0, this.copyW, this.copyH );
     canvasContext.drawImage(canvas, this.srcX, this.srcY, this.copyW, this.copyH,
       this.destX, this.destY, this.copyW, this.copyH);
-    canvasContext.drawImage(hiddenCanvas, 0, 0, this.copyW, this.copyH,
-      this.srcX, this.srcY, this.copyW, this.copyH);
+    // canvasContext.drawImage(hiddenCanvas, 0, 0, this.copyW, this.copyH,
+    //   this.srcX, this.srcY, this.copyW, this.copyH);
 
-    if(this.blitTimer < 60){
-      this.blitTimer++;
-      console.log(this.blitTimer);
-    } else if (this.blitTimer == 60) {
+    if(this.blitTimer < this.lifespan){
+      this.blitTimer = this.blitTimer + 1;
+      this.destX = this.destX + randomInteger(1, 10)
+      
+    } else if (this.blitTimer == this.lifespan) {
+      this.destX = randomInteger(1, 600);
+      console.log("DestX: " + this.destX)
+      this.lifespan = randomInteger(1, 200);
       colorRect(this.destX, this.destY, this.copyW, this.copyH, "rgba(255,255,255,0.5)");
       this.blitTimer = 0;
     }
